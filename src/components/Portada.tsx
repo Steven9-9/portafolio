1ef336b04b07b50278a3627688f40eeaa61af41b
steven garcia <aprendizeco2@gco.com.co>
2025-04-16 11:50:05 -0500

    se arregla menu de opciones

--- src/components/Portada.tsx
+++ src/components/Portada.tsx
@@ -4,8 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import { Navigation, Pagination } from 'swiper/modules';
-import fotoPerfil from '../assets/img/foto5_fotor.png';
-import fotoPerfil2 from '../assets/img/foto4fondonarana.jpg';
+import fotoPerfil from '../assets/img/Portfolio_Presentation_01.jpg';
+import fotoPerfil2 from '../assets/img/Portfolio_Presentation_02.jpg';
 import fotoPerfil3 from '../assets/img/foto6.jpeg';
 import CambioFondo from './CambioFondo';
 
@@ -36,47 +36,43 @@ const Portada: React.FC = () => {
             <section className="photo">
               <img src={fotoPerfil} alt="Foto de perfil" className="photo-img" />
             </section>
-            <div className="welcome">
+            {/* <div className="welcome text1">
               <p className="saludo">
                 <h5>Hola, soy</h5>
                 <h2>
-                  JOHN STEVEN GARCIA ROJAS <br /> Soy asistente en Desarrollo de Software
+                  JOHN STEVEN GARCIA ROJAS <br /> Soy Asistente en Desarrollo de Software
                 </h2>
-                <h4>
-                  Mi pasión por la programación me ha llevado a especializarme en el desarrollo de aplicaciones
-                  web, utilizando las últimas tecnologías y mejores prácticas de la industria.
-                  Estoy constantemente aprendiendo y adaptándome a nuevas tecnologías para mantenerme al día en este
-                  campo en rápida evolución. Si estás buscando un desarrollador comprometido y apasionado,
-                  <br /> ¡no dudes en contactarme!
-                </h4>
               </p>
-            </div>
+            </div> */}
           </SwiperSlide>
           {/* Slide 2 */}
-          <SwiperSlide className='slide2'>
-            <section className="photo2">
-              <img src={fotoPerfil2} alt="Otra imagen" className="photo-img2" />
+          <SwiperSlide>
+            <section className="photo">
+              <img src={fotoPerfil2} alt="Otra imagen" className="photo-img" />
             </section>
-            <div className="welcome">
-              <p className="saludo">
-                <h5>Slide 2</h5>
-                <h2>Otro Título</h2>
-                <h4>Descripción para el segundo slide.</h4>
+            {/* <div className="welcome">
+              <p className="saludo text2">
+                <h5>CONOCE</h5>
+                <h2>UN POCO SOBRE MI</h2>
+                <h4>       
+                Soy técnico en el ámbito tecnológico con sólidos conocimientos en software de vanguardia como Vtex IO, React, MySQL entre otros lenguajes de programación. Mi experiencia se ha centrado en el rol de asistente en desarrollo frontend, donde he demostrado un fuerte compromiso con mis responsabilidades laborales y un elevado sentido humano. Destaco por mi disposición constante hacia el aprendizaje y mi integridad, caracterizándome por ser una persona honesta, paciente y sumamente organizada. Poseo una notable habilidad para priorizar tareas, lo que me permite generar soluciones ágiles y efectivas. Mi orientación al logro y mi capacidad para trabajar en equipo se complementan con mi habilidad para seguir normativas y mi competencia en comunicación oral y escrita, elementos que considero fundamentales para el éxito en cualquier proyecto.
+                  
+                </h4>
               </p>
-            </div>
+            </div> */}
           </SwiperSlide>
           {/* Slide 3 */}
-          <SwiperSlide className='slide3'>
-            <section className="photo3">
-              <img src={fotoPerfil3} alt="Tercera imagen" className="photo-img3" />
+          <SwiperSlide>
+            <section className="photo">
+              <img src={fotoPerfil3} alt="Tercera imagen" className="photo-img" />
             </section>
-            <div className="welcome">
+            {/* <div className="welcome">
               <p className="saludo">
                 <h5>Slide 3</h5>
                 <h2>Más Información</h2>
                 <h4>Detalles adicionales en este slide.</h4>
               </p>
-            </div>
+            </div> */}
           </SwiperSlide>
         </Swiper>
         {/* Botones de navegación personalizados */}
